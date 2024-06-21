27318e0b384bd415fa3fac0e886063c270954c87
PolyGon-13 <hyundiego@hanyang.ac.kr>
2024-06-22 01:04:44 +0900

    ros file

--- arduino_ros/build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ arduino_ros/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -19,48 +19,5 @@ void setup()
 
 void loop()
 {
-    int green = random(0, 256);
-    int blue = random(0, 256);
-    int red = random(256);
 
-    while(bluetooth.available())
-    {
-        char c = bluetooth.read();
-        receiveText += c;
-        delay(5);
-    }
-
-    if(receiveText.length()>0)
-    {
-        if(receiveText.indexOf("blue")!=-1)
-        {
-            analogWrite(11, 255);
-            analogWrite(10, 0);
-            analogWrite(9, 0);
-            delay(100);
-        }
-        else if(receiveText.indexOf("green")!=-1)
-        {
-            analogWrite(10, 255);
-            analogWrite(9, 0);
-            analogWrite(11, 0);
-            delay(100);
-        }
-        else if(receiveText.indexOf("red")!=-1)
-        {
-            analogWrite(9, 255);
-            analogWrite(10, 0);
-            analogWrite(11, 0);
-            delay(100);
-        }
-        else
-        {
-            analogWrite(9, 0);
-            analogWrite(10, 0);
-            analogWrite(11, 0);
-            delay(1000);
-        }
-    }
-
-    receiveText = "";
 }
